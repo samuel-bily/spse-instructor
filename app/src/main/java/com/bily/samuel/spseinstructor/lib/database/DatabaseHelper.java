f97efe7bf20db67355fab01b98bea3b2bb25b8a3
Samuel Bilý <samobily1@gmail.com>
2016-03-14 15:25:11 +0100

    Database Helper upgrade

--- app/src/main/java/com/bily/samuel/spseinstructor/lib/database/DatabaseHelper.java
+++ app/src/main/java/com/bily/samuel/spseinstructor/lib/database/DatabaseHelper.java
@@ -16,13 +16,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "quiz";
     private static final String TABLE_USER = "user";
+    private static final String TABLE_STAT_TESTS = "stat_tests";
+    private static final String TABLE_STAT_USERS = "stat_users";
+    private static final String TABLE_STAT_QUESTIONS = "stat_questions";
 
     private static final String KEY_ID = "id";
     private static final String KEY_IDU = "id_u";
+    private static final String KEY_IDT = "id_t";
     private static final String KEY_NAME = "name";
     private static final String KEY_EMAIL = "email";
+    private static final String KEY_ANSWER = "answer";
+    private static final String KEY_STAT = "stat";
+    private static final String KEY_RIGHT = "right";
 
     private static final String CREATE_TABLE_USER = "CREATE TABLE " + TABLE_USER +  " (" + KEY_ID + " INTEGER PRIMARY KEY,"+ KEY_IDU + " INTEGER," + KEY_NAME + " TEXT," + KEY_EMAIL + " TEXT" + ")";
+
+    private static final String CREATE_TABLE_STAT_TESTS = "CREATE TABLE " + TABLE_STAT_TESTS + " (" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_IDT + " INTEGER," + KEY_NAME + " TEXT" + ")";
+    private static final String CREATE_TABLE_STAT_USERS = "CREATE TABLE " + TABLE_STAT_USERS + " (" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_IDU + " INTEGER," + KEY_NAME + " TEXT," + KEY_STAT + "REAL" + ")";
+    private static final String CREATE_TABLE_STAT_QUESTIONS = "CREATE TABLE " + TABLE_STAT_QUESTIONS + " (" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_RIGHT + " INTEGER," + KEY_NAME + " TEXT," + KEY_ANSWER + " TEXT," + KEY_STAT + "REAL" + ")";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -31,6 +42,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_USER);
+        db.execSQL(CREATE_TABLE_STAT_QUESTIONS);
+        db.execSQL(CREATE_TABLE_STAT_TESTS);
+        db.execSQL(CREATE_TABLE_STAT_USERS);
     }
 
     @Override
